b26c99b1866915bb0e27f5c04ae07b0647091620
TranHai12102003 <trantrunghai141@gmail.com>
2025-03-26 13:02:17 +0700

    Api_V3
    API_V3

---
+++ WebPizza_API_BackEnd/WebPizza_API_BackEnd/Service/IService/ICategoryService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OA.Domain.Common.Models;
+using WebPizza_API_BackEnd.Common.Models;
+using WebPizza_API_BackEnd.VModel;
+
+namespace WebPizza_API_BackEnd.Service.IService
+{
+    public interface ICategoryService
+    {
+        Task<ActionResult<PaginationModel<CategoryGetVModel>>> GetAll();
+        Task<ActionResult<CategoryGetVModel>?> GetbyId(int id);
+        Task<ResponseResult> Create(CategoryCreateVModel model);
+        Task<ResponseResult> Update(CategoryUpdateVModel model);
+        Task<ResponseResult> Remove(int id);
+    }
+}
