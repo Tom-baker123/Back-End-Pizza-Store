b4f2ce19e02fbf141bb1277427efc7334172b45d
TranHai12102003 <trantrunghai141@gmail.com>
2025-04-07 20:48:52 +0700

    api/Payment
    VnPay

--- WebPizza_API_BackEnd/WebPizza_API_BackEnd/Controllers/PaymentController.cs
+++ WebPizza_API_BackEnd/WebPizza_API_BackEnd/Controllers/PaymentController.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.AspNetCore.Cors;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WebPizza_API_BackEnd.Common.Models;
+using WebPizza_API_BackEnd.Context;
+using WebPizza_API_BackEnd.Entities;
 using WebPizza_API_BackEnd.Service.IService;
 
 namespace WebPizza_API_BackEnd.Controllers
@@ -11,16 +15,36 @@ namespace WebPizza_API_BackEnd.Controllers
     public class PaymentController : ControllerBase
     {
         private readonly IVnPayService _vnPayService;
-        public PaymentController(IVnPayService vnPayService)
+        private readonly AppDbContext _context;
+        public PaymentController(IVnPayService vnPayService,AppDbContext context)
         {
             _vnPayService = vnPayService;
+            _context = context;
         }
+        //[HttpPost]
+        //public IActionResult CreatePaymentUrlVnpay(PaymentInfomationModel model)
+        //{
+        //    var url = _vnPayService.CreatePaymentUrl(model, HttpContext);
+        //    if(url == null)
+        //    {
+        //        return BadRequest("Lỗi tạo url thanh toán");
+        //    }
+        //    return Ok(url);
+        //}
         [HttpPost]
-        public IActionResult CreatePaymentUrlVnpay(PaymentInfomationModel model)
+        public async Task<IActionResult> CreatePaymentUrlVnpay(int orderId)
         {
-            var url = _vnPayService.CreatePaymentUrl(model, HttpContext);
-
-            return Redirect(url);
+            var order = await _context.Orders.FindAsync(orderId);
+            if (order == null)
+            {
+                return NotFound("Đơn hàng không tồn tại");
+            }
+            var url = _vnPayService.CreatePaymentUrl(order, HttpContext);
+            if (url == null)
+            {
+                return BadRequest("Lỗi tạo url thanh toán");
+            }
+            return Ok(url);
         }
         [HttpGet]
         public IActionResult PaymentCallbackVnpay()
--- WebPizza_API_BackEnd/WebPizza_API_BackEnd/Service/IService/IVnPayService.cs
+++ WebPizza_API_BackEnd/WebPizza_API_BackEnd/Service/IService/IVnPayService.cs
@@ -1,10 +1,12 @@
 ﻿using WebPizza_API_BackEnd.Common.Models;
+using WebPizza_API_BackEnd.Entities;
 
 namespace WebPizza_API_BackEnd.Service.IService
 {
     public interface IVnPayService
     {
-        string CreatePaymentUrl(PaymentInfomationModel model, HttpContext context);
+        //string CreatePaymentUrl(PaymentInfomationModel model, HttpContext context);
+        string CreatePaymentUrl(Order model, HttpContext context);
         PaymentResponseModel PaymentExecute(IQueryCollection collections);
 
     }
